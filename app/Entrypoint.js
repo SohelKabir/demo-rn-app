a47974516f6307403c1e26900e56c5ec29379283
imsohelkabir <nurul@ihealthscreen.org>
2021-04-01 20:04:23 +0600

    after setup

---
+++ app/Entrypoint.js
@@ -0,0 +1,52 @@
+/**
+ * React Native App
+ * Everything starts from the entrypoint
+ */
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Provider, useSelector } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import {
+  DefaultTheme as NavigationDefaultTheme,
+  DarkTheme as NavigationDarkTheme,
+} from '@react-navigation/native';
+
+import Navigator from 'app/navigation';
+import configureStore from 'app/store/configureStore';
+
+const CombinedDefaultTheme = {
+  ...PaperDefaultTheme,
+  ...NavigationDefaultTheme,
+};
+const CombinedDarkTheme = {
+  ...PaperDarkTheme,
+  ...NavigationDarkTheme,
+};
+
+const { persistor, store } = configureStore();
+
+export function RootNavigation() {
+  const isDark = useSelector(state => state.themeReducer.isDark);
+  const theme = isDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  return (
+    <PaperProvider theme={theme}>
+      <Navigator theme={theme} />
+    </PaperProvider>
+  );
+}
+
+export default function Entrypoint() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <RootNavigation />
+      </PersistGate>
+    </Provider>
+  );
+}
---
+++ app/navigation/NavigationStack.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useSelector } from 'react-redux';
+
+import { navigationRef } from './NavigationService';
+
+import Login from 'app/screens/Login';
+import Home from 'app/screens/Home';
+import ThemeController from '../components/ThemeController';
+import { StatusBar } from 'react-native';
+
+const Stack = createStackNavigator();
+
+const homeOptions = {
+  title: 'Home',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerRight: () => <ThemeController />,
+};
+
+function App(props) {
+  const { theme } = props;
+  const isLoggedIn = useSelector(state => state.loginReducer.isLoggedIn);
+
+  return (
+    <NavigationContainer ref={navigationRef} theme={theme}>
+      <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
+
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={homeOptions} />
+
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            // When logging out, a pop animation feels intuitive
+            // You can remove this if you want the default 'push' animation
+            animationTypeForReplace: isLoggedIn ? 'push' : 'pop',
+            headerRight: () => <ThemeController />,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
